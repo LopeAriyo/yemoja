d4ed2d9db6d32944979b0464522ee2f95eef9c51
Lope Ariyo <lopeariyo@gmail.com>
2020-05-17 21:58:30 +0100

    hid calendar page

--- src/layout/components/navigation/Header.js
+++ src/layout/components/navigation/Header.js
@@ -18,7 +18,7 @@ const Header = props => {
             <div className="spacer"></div>
             <h3 className="page-title">{document.title}</h3>
             <div className="spacer"></div>
-            <div className="info-btn">?</div>
+            {/* <div className="info-btn">?</div> */}
         </header>
     );
 };
