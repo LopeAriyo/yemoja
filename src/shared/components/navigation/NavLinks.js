4fcea6dfe2196582e9f7e540f4dab3321c94d9c3
Lope Ariyo <lopeariyo@gmail.com>
2020-05-21 18:56:08 +0100

    Added sign out functionality, will move to profile page later

--- src/shared/components/navigation/NavLinks.js
+++ src/shared/components/navigation/NavLinks.js
@@ -1,7 +1,11 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+// import { NavLink } from "react-router-dom";
+
+import AuthContext from "../../context/auth-context";
 
 const NavLinks = props => {
+    const auth = useContext(AuthContext);
+
     return (
         <nav className="main-navigation">
             <ul className="nav-links">
@@ -11,17 +15,20 @@ const NavLinks = props => {
                 {/* <li className="nav-link">
                         <Link to="/#contact">Journal</Link>
                     </li> */}
-                <li className="nav-link">
+                {/* <li className="nav-link">
                     <NavLink to="/" exact>
                         Cycle
                     </NavLink>
-                </li>
+                </li> */}
                 {/* <li className="nav-link">
                         <Link to="/#blog">Insights</Link>
                     </li> */}
                 {/* <li className="nav-link">
                     <NavLink to="/profile">Profile </NavLink>
                 </li> */}
+                <li className="nav-link" onClick={auth.signOut}>
+                    Sign Out
+                </li>
             </ul>
         </nav>
     );
